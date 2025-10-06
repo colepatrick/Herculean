44736c57da11a6f9aa8fb0d34ab094fa6cdfed67
kee <seankee8@gmail.com>
2025-10-06 15:49:31 -0500

    Added basic user account object with getters and setters

---
+++ app/src/main/java/com/example/herculean/RegisterAccount.java
@@ -0,0 +1,4 @@
+package com.example.herculean;
+
+public class RegisterAccount {
+}
--- app/src/main/java/com/example/herculean/UserAccount.java
+++ app/src/main/java/com/example/herculean/UserAccount.java
@@ -3,4 +3,52 @@ package com.example.herculean;
 import java.io.Serializable;
 
 public class UserAccount implements Serializable {
+    private String username, password, email;
+    private int level;
+
+    public UserAccount(String username, String password, String email){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.level = 1;
+    }
+
+    /**************************************************************************************
+     * Getters and Setters
+     **************************************************************************************/
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+
+
+
 }
