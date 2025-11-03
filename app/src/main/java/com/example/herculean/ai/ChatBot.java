d47330c40914f76b1223b8144a39922d951f0c0d
kee <seankee8@gmail.com>
2025-11-03 16:26:55 -0600

    Chatbot xml is finished

--- app/src/main/java/com/example/herculean/ai/ChatBot.java
+++ app/src/main/java/com/example/herculean/ai/ChatBot.java
@@ -1,10 +1,22 @@
 package com.example.herculean.ai;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.herculean.R;
 
-public class ChatBot {
+public class ChatBot extends AppCompatActivity {
 
+    private Button buttomHome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_bot);
+
+
+    }
 }
 
