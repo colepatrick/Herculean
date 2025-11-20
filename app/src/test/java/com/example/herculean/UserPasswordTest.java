f4ab7a730a0f502bfd920ee4679439da71a5ddb1
kee <seankee8@gmail.com>
2025-11-20 10:26:20 -0600

    Proper hashing test cases added

--- app/src/test/java/com/example/herculean/UserPasswordTest.java
+++ app/src/test/java/com/example/herculean/UserPasswordTest.java
@@ -1,8 +1,25 @@
 package com.example.herculean;
 
+import com.example.herculean.datahandling.UserAccount;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class UserPasswordTest {
+
+    @Test
+    public void passwordHashing() {
+        UserAccount user = new UserAccount("testUser", "pass123", "test@gmail.com");
+        assertNotEquals("pass123", user.getPassword());
+        assertNotNull(user.getSalt());
+        assertFalse(user.getSalt().isEmpty());
+        assertTrue(user.checkPassword("pass123"));
+    }
+
+    @Test
+    public void incorrectPassword() {
+        UserAccount user = new UserAccount("testUser", "pass123", "test@gmail.com");
+        assertFalse(user.checkPassword("wrongPassword"));
+    }
 }
