b996225f93e2453337abe031c52ec7a780d7a86d
kee <seankee8@gmail.com>
2025-11-21 13:53:42 -0600

    Added more test cases to ensure unique hashing and password changing

--- app/src/test/java/com/example/herculean/UserPasswordTest.java
+++ app/src/test/java/com/example/herculean/UserPasswordTest.java
@@ -22,4 +22,26 @@ public class UserPasswordTest {
         UserAccount user = new UserAccount("testUser", "pass123", "test@gmail.com");
         assertFalse(user.checkPassword("wrongPassword"));
     }
+
+    @Test
+    public void samePassword() {
+        UserAccount user1 = new UserAccount("user1", "commonPass", "test1@gmail.com");
+        UserAccount user2 = new UserAccount("user2", "commonPass", "test2@gmail.com");
+        assertNotEquals(user1.getSalt(), user2.getSalt());
+        assertNotEquals(user1.getPassword(), user2.getPassword());
+        assertTrue(user1.checkPassword("commonPass"));
+        assertTrue(user2.checkPassword("commonPass"));
+    }
+
+    @Test
+    public void passwordChange() {
+        UserAccount user = new UserAccount("testUser", "pass123", "test@gmail.com");
+        String initialHash = user.getPassword();
+        String initialSalt = user.getSalt();
+        user.setPassword("newPass456");
+        assertNotEquals(initialHash, user.getPassword());
+        assertNotEquals(initialSalt, user.getSalt());
+        assertTrue(user.checkPassword("newPass456"));
+        assertFalse(user.checkPassword("pass123"));
+    }
 }
