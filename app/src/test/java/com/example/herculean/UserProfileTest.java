e066faa9c33a716f25d3211948594eab48289cd0
kee <seankee8@gmail.com>
2025-12-04 02:13:16 -0600

    Fixed new workout test cases

--- app/src/test/java/com/example/herculean/UserProfileTest.java
+++ app/src/test/java/com/example/herculean/UserProfileTest.java
@@ -5,7 +5,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import com.example.herculean.datahandling.UserAccount;
+import com.example.herculean.workout.Bodyweight;
+import com.example.herculean.workout.Cardio;
 import com.example.herculean.workout.Logger;
+import com.example.herculean.workout.Strength;
 import com.example.herculean.workout.Workout;
 
 public class UserProfileTest {
@@ -24,10 +27,10 @@ public class UserProfileTest {
         UserAccount testAccount = new UserAccount();
 
         Logger testLog = new Logger();
-        Workout best = new Workout("testWorkout1", "testBodyPart1", 5, 5, 100);
+        Workout best = new Strength("testWorkout1", "testBodyPart1", 5, 5, 100);
 
         testLog.addWorkout(best);
-        testLog.addWorkout(new Workout("testWorkout2", "testBodyPart2", 5, 4, 100)); // Less weight
+        testLog.addWorkout(new Strength("testWorkout2", "testBodyPart2", 5, 4, 100));
 
         testAccount.setWorkoutLog(testLog);
         assertEquals(best, testAccount.getBestWorkout());
@@ -38,14 +41,14 @@ public class UserProfileTest {
         UserAccount testAccount = new UserAccount();
 
         Logger testLog = new Logger();
-        Workout best = new Workout("testWorkout1", "testBodyPart1", 5, 5, 100);
+        Workout best = new Strength("testWorkout1", "testBodyPart1", 5, 5, 100);
 
         testLog.addWorkout(best);
-        testLog.addWorkout(new Workout("testWorkout2", "testBodyPart2", 5, 4, 100));
-        testLog.addWorkout(new Workout("testWorkout2", "testBodyPart2", 1, 2, 100));
-        testLog.addWorkout(new Workout("testWorkout2", "testBodyPart2", 1, 9, 100));
-        testLog.addWorkout(new Workout("testWorkout3", "testBodyPart3", 6, 3, 100));
-        testLog.addWorkout(new Workout("testWorkout3", "testBodyPart3", 6, 3, 100));
+        testLog.addWorkout(new Strength("testWorkout2", "testBodyPart2", 5, 4, 100));
+        testLog.addWorkout(new Strength("testWorkout2", "testBodyPart2", 1, 2, 100));
+        testLog.addWorkout(new Strength("testWorkout2", "testBodyPart2", 1, 9, 100));
+        testLog.addWorkout(new Strength("testWorkout3", "testBodyPart3", 6, 3, 100));
+        testLog.addWorkout(new Strength("testWorkout3", "testBodyPart3", 6, 3, 100));
 
         testAccount.setWorkoutLog(testLog);
         assertEquals(best, testAccount.getBestWorkout());
@@ -66,8 +69,8 @@ public class UserProfileTest {
         UserAccount testAccount = new UserAccount();
 
         Logger testLog = new Logger();
-        testLog.addWorkout(new Workout("testWorkout1", "testBodyPart1", 5, 5, 100));
-        testLog.addWorkout(new Workout("testWorkout2", "testBodyPart2", 5, 4, 100)); // Less weight
+        testLog.addWorkout(new Strength("testWorkout1", "testBodyPart1", 5, 5, 100));
+        testLog.addWorkout(new Strength("testWorkout2", "testBodyPart2", 5, 4, 100));
 
         testAccount.setWorkoutLog(testLog);
         assertEquals("testWorkout1", testAccount.getFavoriteWorkoutType());
@@ -78,9 +81,9 @@ public class UserProfileTest {
         UserAccount testAccount = new UserAccount();
 
         Logger testLog = new Logger();
-        testLog.addWorkout(new Workout("testWorkout1", "testBodyPart1", 5, 5, 100));
-        testLog.addWorkout(new Workout("testWorkout2", "testBodyPart2", 5, 4, 100)); // Less weight
-        testLog.addWorkout(new Workout("testWorkout2", "testBodyPart2", 5, 2, 100)); // Now more weight
+        testLog.addWorkout(new Strength("testWorkout1", "testBodyPart1", 5, 5, 100));
+        testLog.addWorkout(new Strength("testWorkout2", "testBodyPart2", 5, 4, 100));
+        testLog.addWorkout(new Strength("testWorkout2", "testBodyPart2", 5, 2, 100));
 
         testAccount.setWorkoutLog(testLog);
         assertEquals("testWorkout2", testAccount.getFavoriteWorkoutType());
@@ -102,8 +105,8 @@ public class UserProfileTest {
         UserAccount testAccount = new UserAccount();
 
         Logger testLog = new Logger();
-        testLog.addWorkout(new Workout("testWorkout1", "testBodyPart1", 5, 5, 100));
-        testLog.addWorkout(new Workout("testWorkout2", "testBodyPart2", 5, 4, 100)); // Less weight
+        testLog.addWorkout(new Strength("testWorkout1", "testBodyPart1", 5, 5, 100));
+        testLog.addWorkout(new Strength("testWorkout2", "testBodyPart2", 5, 4, 100));
 
         testAccount.setWorkoutLog(testLog);
         assertEquals("testBodyPart1", testAccount.getFavoriteMuscleGroup());
@@ -114,11 +117,89 @@ public class UserProfileTest {
         UserAccount testAccount = new UserAccount();
 
         Logger testLog = new Logger();
-        testLog.addWorkout(new Workout("testWorkout1", "testBodyPart1", 5, 5, 100));
-        testLog.addWorkout(new Workout("testWorkout2", "testBodyPart2", 5, 4, 100)); // Less weight
-        testLog.addWorkout(new Workout("testWorkout2", "testBodyPart2", 5, 2, 100)); // Now more weight
+        testLog.addWorkout(new Strength("testWorkout1", "testBodyPart1", 5, 5, 100));
+        testLog.addWorkout(new Strength("testWorkout2", "testBodyPart2", 5, 4, 100));
+        testLog.addWorkout(new Strength("testWorkout2", "testBodyPart2", 5, 2, 100));
 
         testAccount.setWorkoutLog(testLog);
         assertEquals("testBodyPart2", testAccount.getFavoriteMuscleGroup());
+    }
+
+    // Cardio Tests
+    @Test
+    public void getBestWorkoutCardio() {
+        UserAccount testAccount = new UserAccount();
+
+        Logger testLog = new Logger();
+        Workout best = new Cardio("Running", "Legs", 30, 3);
+
+        testLog.addWorkout(best);
+        testLog.addWorkout(new Cardio("Cycling", "Legs", 60, 5));
+
+        testAccount.setWorkoutLog(testLog);
+        assertEquals(best, testAccount.getBestWorkout());
+    }
+
+    @Test
+    public void getFavoriteWorkoutTypeCardio() {
+        UserAccount testAccount = new UserAccount();
+
+        Logger testLog = new Logger();
+        testLog.addWorkout(new Cardio("Running", "Legs", 30, 3));
+        testLog.addWorkout(new Cardio("Cycling", "Legs", 60, 5));
+
+        testAccount.setWorkoutLog(testLog);
+        assertEquals("Running", testAccount.getFavoriteWorkoutType());
+    }
+
+    @Test
+    public void getFavoriteMuscleGroupCardio() {
+        UserAccount testAccount = new UserAccount();
+
+        Logger testLog = new Logger();
+        testLog.addWorkout(new Cardio("Running", "Legs", 30, 3));
+        testLog.addWorkout(new Cardio("Swimming", "Full Body", 60, 1));
+
+        testAccount.setWorkoutLog(testLog);
+        assertEquals("Legs", testAccount.getFavoriteMuscleGroup());
+    }
+
+    // Bodyweight Tests
+    @Test
+    public void getBestWorkoutBodyweight() {
+        UserAccount testAccount = new UserAccount();
+
+        Logger testLog = new Logger();
+        Workout best = new Bodyweight("Pushups", "Chest", 3, 20);
+
+        testLog.addWorkout(best);
+        testLog.addWorkout(new Bodyweight("Pullups", "Back", 5, 5));
+
+        testAccount.setWorkoutLog(testLog);
+        assertEquals(best, testAccount.getBestWorkout());
+    }
+
+    @Test
+    public void getFavoriteWorkoutTypeBodyweight() {
+        UserAccount testAccount = new UserAccount();
+
+        Logger testLog = new Logger();
+        testLog.addWorkout(new Bodyweight("Pushups", "Chest", 3, 20));
+        testLog.addWorkout(new Bodyweight("Situps", "Abs", 5, 25));
+
+        testAccount.setWorkoutLog(testLog);
+        assertEquals("Situps", testAccount.getFavoriteWorkoutType());
+    }
+
+    @Test
+    public void getFavoriteMuscleGroupBodyweight() {
+        UserAccount testAccount = new UserAccount();
+
+        Logger testLog = new Logger();
+        testLog.addWorkout(new Bodyweight("Pushups", "Chest", 3, 20));
+        testLog.addWorkout(new Bodyweight("Squats", "Legs", 5, 25));
+
+        testAccount.setWorkoutLog(testLog);
+        assertEquals("Legs", testAccount.getFavoriteMuscleGroup());
     }
 }
